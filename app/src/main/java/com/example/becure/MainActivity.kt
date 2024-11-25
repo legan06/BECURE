c63e6009d2c4b32a5964978dca46079ce9c771e3
unknown <murat.salman@ug.bilkent.edu.tr>
2024-11-25 19:48:54 +0300

    İkinci commit denemesi

--- app/src/main/java/com/example/becure/MainActivity.kt
+++ app/src/main/java/com/example/becure/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    //deneme editi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
